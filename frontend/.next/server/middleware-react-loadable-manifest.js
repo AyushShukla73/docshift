b3cdbd92e075378a148c03806c227f96c6261da6
AyushShukla73 <ayushshukla240104@gmail.com>
2026-07-09 05:56:06 +0530

    Remove generated files and environments from tracking

--- frontend/.next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"features\\\\workspace\\\\SourcePanel.tsx -> @/lib/fileDetection\":{\"id\":\"features\\\\workspace\\\\SourcePanel.tsx -> @/lib/fileDetection\",\"files\":[]}}"
